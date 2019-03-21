64316115f1159fc4689b4309763e7d52aa6dd4de
Shafira Ananda <afishafiraa@gmail.com>
2019-03-21 08:25:23 +0700

    Updates

---
+++ precache-manifest.ec7e043064510bd3ac9c30732706febd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "e84239c7e7c0d55e7593",
+    "url": "/website/static/js/main.25f690a3.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/website/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "e84239c7e7c0d55e7593",
+    "url": "/website/static/css/main.55fa278a.chunk.css"
+  },
+  {
+    "revision": "dbc69decc7d6e0c901da6d70abaaceaa",
+    "url": "/website/index.html"
+  }
+];
