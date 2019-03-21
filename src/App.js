9643491da724578bc562cb04ba4efae29c0c1011
Shafira Ananda <afishafiraa@gmail.com>
2019-03-21 08:22:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,23 +3,80 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      color: 'Merah',
+      img   : 'apple.jpg',
+      title : 'Apel',
+    };
+  }
+
+  clicked(menu){
+    //untuk memperbarui state sesuai dengan menu yang dipilih
+    
+    this.setState({
+      color : menu.color,
+      img   : menu.img,
+      title : menu.title,
+    });
+  }
+  /* gambarku(){
+    var gambar;
+    if (this.state.active.toLocaleLowerCase()==='merah'){
+      gambar = "apel";
+    } else if (this.state.active.toLocaleLowerCase()=== 'kuning' ){
+      gambar ="nanas";
+    } else if (this.state.active.toLocaleLowerCase() === 'hijau'){
+      gambar ="alpukat";
+    } else if (this.state.active.toLocaleLowerCase() === 'biru'){
+      gambar ="berry";
+    }
+
+    return <img src={gambar+".jpg"} width="200"></img>
+  }
+*/
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      {/*map akan loop sebanyak menu yang di definisikan*/}
+      {/* kemudian mengembalikan elemen </a> */}
+      
+      <nav className="nav">
+      { this.props.items.map ((menu, index) => {
+        var style = 'menu';
+        
+        if (this.state.color === menu.color){
+          style = `${style} is-active`;
+        }
+
+        return <a 
+          className={style+" "+menu.color}
+
+          //bind untuk membuat 'menu' bisa dikirim ke fungsi 'clicked'
+          onClick={this.clicked.bind(this,menu)}
+          key={index}
           >
-            Learn React
-          </a>
-        </header>
+          {menu.title}   
+          </a>;
+          //judul pada menu
+      })  }
+      
+      </nav>
+
+        <div className="info">
+          ini adalah <span className={"selected " + this.state.color}> {this.state.title}</span>
+          <br/> 
+          <br/>
+
+          <img src={this.state.img} width="200"></img>
+          {/*this.gambarku()}
+          */}
+
+        </div>
+        
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const items = [
+    {
+        color   :'Merah',
+        img     : 'apple.jpg',
+        title   :'Apel',
+    },
+    {
+        color   :'Kuning',
+        img     :'nanas.jpg',
+        title   :'Nanas',
+    },
+    {
+        color   :'Hijau',
+        img     :'alpukat.jpg',
+        title   :'Alpukat',
+    },
+    {
+        color   :'Biru',
+        img     :'berry.jpg',
+        title   :'Blueberry',
+    },
+];
+
+ReactDOM.render(<App items={items} />, 
+    document.getElementById('root'));
+
